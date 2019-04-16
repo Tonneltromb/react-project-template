5c4dae8e8001ad841f7051216673b9b1c49e9e27
ymatin <MatinYA@lad24.ru>
2019-04-16 17:09:00 +0300

    реализовал компонент выпадающего списка с анимацией каждого элемента

--- src/components/NavMenu/NavItem/NavItem.js
+++ src/components/NavMenu/NavItem/NavItem.js
@@ -1,18 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import './NavItem.css'
+import CSSTransition from "react-transition-group/CSSTransition";
+
+export const DropStrategy = {top: 1, bottom: 'bottom', left: 'left', right: 4};
 
 const NavItem = (props) => {
+    const [onHover, setOnHover] = useState(false);
+    const [onMenuEntered, setOnMenuEntered] = useState(false);
     return (
-        <div className='NavItem'>
+        <div className={props.itemClass}
+             onMouseOver={() => setOnHover(true)}
+             onMouseLeave={() => setOnMenuEntered(false)}
+        >
+            {props.title}
+            {props.dropMenuItems && props.dropMenuItems.length
+                ? (
+                    <CSSTransition
+                        mountOnEnter
+                        unmountOnExit
+                        in={onHover}
+                        timeout={100}
+                        onEntered={() => setOnMenuEntered(true)}
+                        classNames={{
+                            enterActive: 'show-expand-menu-button',
+                            exitActive: 'hide-expand-menu-button'
+                        }}>
+                        <div className={`NavItem__dropMenu ${props.dropStrategy}-strategy`}>
+                            {props.dropMenuItems.map((item) => {
+                                return (
+                                    <CSSTransition
+                                        mountOnEnter
+                                        unmountOnExit
+                                        in={onMenuEntered}
+                                        timeout={100}
+                                        onExited={() => setOnHover(false)}
+                                        classNames={{
+                                            enterActive: 'show-expand-menu-button',
+                                            exitActive: 'hide-expand-menu-button'
+                                        }}>
+                                        <NavItem
+                                            itemClass={props.dropMenuItemClass}
+                                            title={item.title}
+                                            dropMenuItemClass={props.dropMenuItemClass}
+                                            dropStrategy={DropStrategy.left}
+                                            dropMenuItems={item.dropMenuItems}/>
+                                    </CSSTransition>
+                                );
+                            })}
 
+                        </div>
+                    </CSSTransition>
+                )
+                : null
+            }
         </div>
     );
 };
 
 NavItem.propTypes = {
-    dropdownItems: PropTypes.array
+    itemClass: PropTypes.string,
+    dropMenuItemClass: PropTypes.string,
+    title: PropTypes.string,
+    navigateTo: PropTypes.string,
+    dropMenuItems: PropTypes.array,
+    dropStrategy: PropTypes.string
 };
+NavItem.defaultProps = {
+    itemClass: 'NavItem',
+    dropMenuItemClass: 'DropMenuNavItem'
+};
+
 
 export default NavItem;
--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -2,19 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './NavMenu.css'
-import NavItem from "./NavItem/NavItem";
+import NavItem, {DropStrategy} from "./NavItem/NavItem";
 
 const NavMenu = (props) => {
+    const dropMenuItems = [
+        {
+            title: '1',
+            navigateTo: '/1',
+            dropMenuItems: [
+                {
+                    title: '3',
+                    navigateTo: '/3'
+                },
+                {
+                    title: '4',
+                    navigateTo: '/4'
+                },
+                {
+                    title: '5',
+                    navigateTo: '/5'
+                }
+            ]
+        },
+        {
+            title: '2',
+            navigateTo: '/2',
+            dropMenuItems: [
+                {
+                    title: '3',
+                    navigateTo: '/3'
+                },
+                {
+                    title: '4',
+                    navigateTo: '/4'
+                }
+            ]
+        }
+    ];
     return (
         <div className='NavMenu'>
-            <NavItem title='На главную' navigateTo='/' />
-            <NavItem title='Новости' navigateTo='/news' />
+            <NavItem
+                title='На главную'
+                navigateTo='/'/>
+            <NavItem
+                title='Новости'
+                navigateTo='/news'/>
+            <NavItem
+                title='Примеры'
+                navigateTo='/examples'
+                dropMenuItems={dropMenuItems}
+                dropStrategy={DropStrategy.bottom}/>
         </div>
     );
 };
 
 NavMenu.propTypes = {
-
+    items: PropTypes.array
 };
 
 export default NavMenu;
